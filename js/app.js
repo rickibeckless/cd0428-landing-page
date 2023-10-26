a5a760ebacef20c56aed9a4580e257c2dd95f91d
Ricki Beckless <glbeckless@hotmail.com>
2023-10-26 17:26:02 -0500

    basic-requirements

--- js/app.js
+++ js/app.js
@@ -23,14 +23,15 @@
  * 
 */
 
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
 
 /**
  * End Helper Functions
@@ -40,12 +41,49 @@
 
 // build the nav
 
+    sections.forEach(section => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        navList.appendChild(listItem);
+    });
 
 // Add class 'active' to section when near top of viewport
 
+    window.addEventListener('scroll', function() {
+        let fromTop = window.scrollY;
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop - 400;
+            const sectionHeight = section.offsetHeight;
+            const listItem = document.querySelector(`a[href="#${section.id}"]`);
+    
+            if (fromTop >= sectionTop && fromTop < sectionTop + sectionHeight) {
+                section.classList.add('active-section');
+                listItem.classList.add('active');
+                listItem.classList.add('active-section-background');
+            } else {
+                section.classList.remove('active-section');
+                listItem.classList.remove('active');
+                listItem.classList.remove('active-section-background');
+            };
+        });
+    });
 
 // Scroll to anchor ID using scrollTO event
-
+    navList.addEventListener('click', function(event) {
+        event.preventDefault();
+  
+        if (event.target.nodeName === 'A') {
+            const targetId = event.target.getAttribute('href').slice(1);
+            const targetSection = document.getElementById(targetId);
+  
+            // Scroll to the target section with smooth behavior
+            targetSection.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
 
 /**
  * End Main Functions
@@ -58,5 +96,3 @@
 // Scroll to section on link click
 
 // Set sections as active
-
-
